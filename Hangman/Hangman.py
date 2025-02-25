85add0c204531ff44e58dda5d7fc2636c9440aaf
anthonySann2003 <anthonyjsannazzaro@gmail.com>
2025-02-24 21:33:03 -0500

    Added basic game logic and branching

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -13,9 +13,16 @@ class Hangman:
         self.checkGuess(guess)
 
     def checkGuess(self, guess):
+        correctGuessFlag = False
         for char in self.secretPhrase:
             if(char == guess): #Guessed a letter correct
                 print('Correct Guess')
+                correctGuessFlag = True
+
+        if(correctGuessFlag):
+            self.updatePhrase(guess)
+        else:
+            self.updateBoard()
 
     def updatePhrase(self, guess):
         #Loop through guessPhrase and change * to correct letter where guessed
@@ -23,11 +30,21 @@ class Hangman:
 
     def updateBoard(self):
         #Update the guessesLeft and hangman ascii
-        print('Guesses Left: ', self.guessesLeft)
+        self.guessesLeft = self.guessesLeft - 1 #Update guesses left
+
+        if(self.guessesLeft == 0):
+            self.gameOver()
+        else:
+            print('Guesses Left: ', self.guessesLeft)
 
     def runGame(self):
         while self.guessesLeft > 0:
             self.getGuess()
+
+    def gameOver(self):
+        #Call this when out of guesses
+        print("Sorry you're out of guesses, you lose!")
+        print("The phrase was:", self.secretPhrase)
 
 userName = input("Enter your name: ")
 h1 = Hangman(userName)
